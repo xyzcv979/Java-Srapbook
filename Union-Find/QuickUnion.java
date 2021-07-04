6275ba27877e9bdc453eb989fe24093b442918ad
Alex Ng <avon1462@gmail.com>
2021-07-04 19:41:22 -0400

    Create QuickUnion.java

---
+++ Union-Find/QuickUnion.java
@@ -0,0 +1,41 @@
+/*
+* Quick Union algorithm (better than quick find)
+* 
+* Initialize:  time
+* Union:  time
+* Find:  time
+*
+* Created by: Alexander Ng
+* Date: July 04, 2021
+*/
+
+
+
+public class QuickUnion{
+    private int[] id;
+    
+    public QuickUnion(int n){
+        id = new id[n];
+        for(int i =0; i<n; i++){
+            id[i] = i;
+        }
+    }
+    
+    private int root(int i){
+        while(i != id[i])
+            i = id[i];
+        return i;
+    }
+    
+    // Checks if roots are equal
+    public boolean connected(int p, int q){
+        return root(p) == root(q);
+    }
+    
+    public void union(int p, int q){
+        // Find roots first
+        int i = root(p);
+        int j = root(q);
+        id[i] = j;
+    }
+}
