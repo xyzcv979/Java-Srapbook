8dc5c9869e2ac7ce1a4301088c5b5902e31574f6
Alex Ng <avon1462@gmail.com>
2021-07-04 19:10:36 -0400

    Update QuickFind.java

--- Union-Find/QuickFind.java
+++ Union-Find/QuickFind.java
@@ -6,3 +6,31 @@
 */
 
 
+public class QuickFind{
+
+    private int[] id;
+
+    // instantiate id array
+    public QuickFind(int n){
+        id = new int[n];
+        for(int i =0; i<n; i++)
+            id[i] = i;
+    }
+
+    // Checks whether p and q are connected
+    public boolean connected(int p, int q){
+        // p and q are connected when they are equal, b/c of how union works
+        return id[p] == id[q];
+    }
+
+    // Connects p and q. Changes all entries connected to p to q
+    public void union(int p, int q){
+        int pId = id[p];
+        int qId = id[q];
+
+        for(int i = 0; i<id.length; i++){
+            if(id[i] == pId)
+                id[i] = qId;
+        }
+    }
+}
