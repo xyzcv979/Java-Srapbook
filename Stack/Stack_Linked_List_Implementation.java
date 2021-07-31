1871c1f2c13b2266d2cb8b6417ae2aeb65689aa7
Alex Ng <avon1462@gmail.com>
2021-07-31 17:18:59 -0400

    Update Stack_Linked_List_Implementation.java

--- Stack/Stack_Linked_List_Implementation.java
+++ Stack/Stack_Linked_List_Implementation.java
@@ -1,1 +1,47 @@
+/*
+* Stack_Linked_List_Implementation.java
+*
+* Creating a stack using a LinkedList
+* Trade Offs: 
+*
+*/
 
+public class Stack_Linked_List_Implementation{
+  
+  private Node head = null;
+  
+  private class Node{
+    Object val;
+    Node next;
+    
+  }
+  
+  public boolean isEmpty(){
+    return head.val == null; 
+  }  
+  
+  public Node pop(){
+    if(head == null)
+      return null;
+    Node temp = head;
+    head = head.next;
+    return temp;
+  }
+    
+    
+  public void push(Object val){
+    Node newNode = new Node();
+    newNode.val = val;
+    newNode.next = head;
+    head = newNode;
+  }
+    
+    
+  public Node peek(){
+    if(head == null)
+      return null;
+    return head; 
+  }
+  
+  
+}
