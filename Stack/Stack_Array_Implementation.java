bba16d350d24db9793dc28c7b9e26fc1e5d04087
Alex Ng <avon1462@gmail.com>
2021-07-31 17:38:29 -0400

    Update Stack_Array_Implementation.java

--- Stack/Stack_Array_Implementation.java
+++ Stack/Stack_Array_Implementation.java
@@ -1,1 +1,53 @@
+/*
+* Stack_Array_Implementation.java
+*
+* Stack using an array
+* Benefits: constant amortized analysis. Over time, better runtime than linkedlist implementation
+* Trade offs: 
+*
+*/
 
+public class Stack_Array_Implementation{
+  private Object[] array;
+  private int stackCount;
+  
+  public Stack_Array_Implementation(){
+    array = new Object[1];
+    stackCount = 1;
+  }
+  public boolean isEmpty(){
+    return array.length == 0 ? true : false; 
+  }
+  public void push(Object item){
+    // Resizing array if it hits threshold
+    if(stackCount == array.length)
+      resize(array.length * 2);
+    array[stackCount++] = item;
+  }
+  
+  private void resize(int capacity){
+    int[] copy = new int[capacity];
+    for(int i = 0; i<array.length; i++){
+      copy[i] = array[i]; 
+    }
+    array = copy;
+  }
+  
+  public Object pop(){
+    if(stackCount == 0)
+      return null;
+    
+    Object temp = array[--stackCount];
+    
+    // shrinking array, saving space
+    if(stackCount > 0 && stackCount == array.length / 4)  
+      resize(array.length / 2);
+    
+    return temp;
+  }
+  
+  public Object peek(){
+    return array[stackCount-1];
+  }
+ 
+}
