85e686410bf6d03a45e7ae8f214db6aa35641c98
Alex Ng <avon1462@gmail.com>
2021-08-01 14:45:38 -0400

    Update SimpleSearch.java

--- Searching/SimpleSearch.java
+++ Searching/SimpleSearch.java
@@ -1,10 +1,10 @@
-# Simple Search
-# Finds an element in array
-#
-# O(n) time complexity
-# O(1) space 
-#
-# Created by xyzcv979
+// Simple Search
+// Finds an element in array
+// 
+// O(n) time complexity
+// O(1) space 
+// 
+// Created by xyzcv979
 
 
 
